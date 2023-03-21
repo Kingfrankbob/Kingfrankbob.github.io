781c0ab3c93a0656edde0dd281c35d180a06bb1c
Kingfrankbob <AJWpcc@gmail.com>
2023-03-21 09:37:39 -0500

    JS updates

--- assets/js/googleRetreive.js
+++ assets/js/googleRetreive.js
@@ -1,12 +1,20 @@
 // using google sheets api to retrive data from google sheets
 console.log("googleRetreive.js loaded");
 gapi.load("client", function () {
-    gapi.client.init({
-        apiKey: "AIzaSyByVmS9mRHs3K1d_M0WXtGg6QC_1H5lOo4",
-        clientId: "418644215361-mg35m20q1d7i82k64utfibpf8e1nrsca.apps.googleusercontent.com",
-        discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-        scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
-    }).then(function () {
+    // gapi.client.init({
+    //     apiKey: "AIzaSyByVmS9mRHs3K1d_M0WXtGg6QC_1H5lOo4",
+    //     clientId: "418644215361-mg35m20q1d7i82k64utfibpf8e1nrsca.apps.googleusercontent.com",
+    //     discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    //     scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
+    // })
+        
+        
+        window.gapi.client
+        .init({
+          clientId:'418644215361-mg35m20q1d7i82k64utfibpf8e1nrsca.apps.googleusercontent.com',
+          scope: "display@project-b-381214.iam.gserviceaccount.com",
+          plugin_name:'project-b-381214'
+        }).then(function () {
         console.log("gapi.client.init() success");
         // var spreadsheetId = "1OwsrJNNC_yWgsHPLPhn3ejGUjmebfZq4yyDnCup9hDw";
         // var range = "Summary!B4";
