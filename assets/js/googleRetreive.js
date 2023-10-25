e0366aa71981505da4f3f82372f00c494b3960c3
Kingfrankbob <AJWpcc@gmail.com>
2023-10-25 10:09:49 -0500

    Made Changes to the Script

--- assets/js/googleRetreive.js
+++ assets/js/googleRetreive.js
@@ -1,14 +1,14 @@
-      const sheetId = '1OwsrJNNC_yWgsHPLPhn3ejGUjmebfZq4yyDnCup9hDw';
-      const sheetName = 'summary';
-      const range = 'B4';
-      const apiKey = 'AIzaSyByVmS9mRHs3K1d_M0WXtGg6QC_1H5lOo4';
-      const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}!${range}?key=${apiKey}`;
+const sheetId = "1OwsrJNNC_yWgsHPLPhn3ejGUjmebfZq4yyDnCup9hDw";
+const sheetName = "summary";
+const range = "B4:B5";
+const apiKey = "AIzaSyByVmS9mRHs3K1d_M0WXtGg6QC_1H5lOo4";
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}!${range}?key=${apiKey}`;
 
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          data.values.forEach(row => {
-            h1 = document.getElementById('data');
-            h1.innerHTML = "Current GHT: " + row[0];
-          });
-        });
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    data.values.forEach((row) => {
+      h1 = document.getElementById("data");
+      h1.innerHTML = "GPAT: " + row[0] + " GAAT: " + row[1];
+    });
+  });
